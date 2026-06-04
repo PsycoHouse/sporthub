05aae49404b344e400cf0ea2b4c25081062c47b2
PsycoHouse <131466298+PsycoHouse@users.noreply.github.com>
2026-06-04 16:44:22 +0200

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBTDz0R8WkY4ov8_28JMpe0jVmSD05oJb4",
+  authDomain: "sport-project-301c2.firebaseapp.com",
+  databaseURL: "https://sport-project-301c2-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "sport-project-301c2",
+  storageBucket: "sport-project-301c2.firebasestorage.app",
+  messagingSenderId: "743420630517",
+  appId: "1:743420630517:web:c99cdf9dcd7699aa37fcbc",
+  measurementId: "G-BG3SE17BB7"
+});
+
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(payload => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/icons/icon-192.png"
+  });
+});
